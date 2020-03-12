748ff79c3c9b3f3a031644b8207f2705fb6b3208
bohong <977106024@qq.com>
2020-03-12 21:52:12 +0800

    php入门练习demo

---
+++ mysql.php
@@ -0,0 +1,22 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$pwd = '';
+$dbname = 'php10';
+
+$db = new mysqli($host,$user,$pwd,$dbname);
+
+if($db->connect_errno <> 0){
+    echo '链接失败';
+    echo $db -> connect_error;
+}
+
+$sql = "insert into msg (content,user,intime) value ('这是php中插入数据','吴',21312321321)";
+$is = $db -> query($sql);
+if($is){
+    echo '插入成功';
+}else{
+    echo '插入失败';
+}
+
+?>
---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+    $data = $_POST['aa'];
+    if($data == 123){
+        echo '正确';
+    }else{
+        echo '错误';
+    }
+    // echo $data;
+?>
