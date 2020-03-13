ed48dcd6a8653f46cf6a52e820e5fc6e6257c5da
bohong <977106024@qq.com>
2020-03-13 14:38:57 +0800

    连接数据库 查询数据

--- mysql.php
+++ mysql.php
@@ -11,12 +11,21 @@ if($db->connect_errno <> 0){
     echo $db -> connect_error;
 }
 
-$sql = "insert into msg (content,user,intime) value ('这是php中插入数据','吴',21312321321)";
-$is = $db -> query($sql);
-if($is){
-    echo '插入成功';
-}else{
-    echo '插入失败';
+$sql = "select * from msg";
+$mysql_result = $db->query($sql);
+
+var_dump($mysql_result->fetch_array(MYSQLI_ASSOC));
+
+while($row = $mysql_result->fetch_array(MYSQLI_ASSOC)){
+    var_dump($row);
 }
+
+// $sql = "insert into msg (content,user,intime) value ('这是php中插入数据','吴',21312321321)";
+// $is = $db -> query($sql);
+// if($is){
+//     echo '插入成功';
+// }else{
+//     echo '插入失败';
+// }
 
 ?>
